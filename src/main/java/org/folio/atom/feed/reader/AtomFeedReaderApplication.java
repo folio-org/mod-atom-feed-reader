9746f60720e86447300d09cb453c54d7bcdeb7d5
askhat-abishev <askhat_abishev@epam.com>
2024-10-23 16:25:59 +0500

    MODLD-542: Create a skeleton for Atom Feed Reader microservice

---
+++ src/main/java/org/folio/atom/feed/reader/AtomFeedReaderApplication.java
@@ -0,0 +1,13 @@
+package org.folio.atom.feed.reader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomFeedReaderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AtomFeedReaderApplication.class, args);
+  }
+
+}
